203379a1d6c99b29490cb146c25afd4520151415
imraj560 <raju560.webdev@gmail.com>
2025-08-12 11:01:06 -0400

    Home Page Card Completed

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,51 @@
 import React from 'react'
 import { Button } from '@/components/ui/button'
+import CompanionCard from '@/components/CompanionCard'
+import CompanionList from '@/components/CompanionList'
+import CTA from '@/components/CTA'
 
 const Page = () => {
   return (
-    <div>
-      <h1 className='text-2xl underline'>Welcome to my Sass App</h1>
-      <Button>Lets Get Started</Button>
-    </div>
+    <main>
+      <h1>Popular Companions</h1>
+
+      <section className='home-section'>
+
+        <CompanionCard
+          id="123"
+          name="Neura the Brainy Explorer"
+          topic="Neural Network of the Brain"
+          subject="science"
+          duration={45}
+          color="#ffda6e"
+        />
+
+        <CompanionCard
+          id="456"
+          name="Countsy the Number Wizard"
+          topic="Derivatives and Itegrals"
+          subject="Maths"
+          duration={30}
+          color="#e5d0ff"
+        />
+
+        <CompanionCard
+          id="789"
+          name="Verba the Vocabulary Builder"
+          topic="language"
+          subject="English Literature"
+          duration={30}
+          color="#BDE7FF"
+        />
+       
+      </section>
+
+      <section className='home-section'>
+        <CompanionList/>
+        <CTA/>
+      </section>
+
+    </main>
   )
 }
 
